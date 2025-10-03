199edb9632966c28318ebcf0cb51a2e87a88e0b5
ggm77 <shmshm86@naver.com>
2025-10-03 17:02:45 +0900

    init : first commit

---
+++ src/main/java/com/treasurehunter/treasurehunter/TreasurehunterApplication.java
@@ -0,0 +1,13 @@
+package com.treasurehunter.treasurehunter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TreasurehunterApplication {
+    // 2025.10.03 develop start
+	public static void main(String[] args) {
+		SpringApplication.run(TreasurehunterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/treasurehunter/treasurehunter/TreasurehunterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.treasurehunter.treasurehunter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TreasurehunterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
