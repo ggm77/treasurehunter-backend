051ebaa75a0f8a144641c2a2e11db3be09929893
ggm77 <shmshm86@naver.com>
2025-10-07 20:14:20 +0900

    fix: adjust JwtAuthFilter order

--- src/main/java/com/treasurehunter/treasurehunter/global/config/WebSecurityConfig.java
+++ src/main/java/com/treasurehunter/treasurehunter/global/config/WebSecurityConfig.java
@@ -1,5 +1,7 @@
 package com.treasurehunter.treasurehunter.global.config;
 
+import com.treasurehunter.treasurehunter.global.auth.filter.JwtAuthenticationFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -7,10 +9,14 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class WebSecurityConfig {
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
     @Bean
     protected SecurityFilterChain configure(final HttpSecurity httpSecurity) throws Exception {
@@ -23,6 +29,8 @@ public class WebSecurityConfig {
                         "/api/swagger/**",
                         "/api/v1/auth/**"//개발중에만 사용
                 ).permitAll().anyRequest().authenticated());
+
+        httpSecurity.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
         return httpSecurity.build();
     }
