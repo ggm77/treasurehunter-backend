9cbfe67eb7206386b45dd55a0add73ba51276529
ggm77 <shmshm86@naver.com>
2025-10-09 15:57:01 +0900

    feat: add oauth accounts info to user response

--- src/main/java/com/treasurehunter/treasurehunter/domain/user/dto/UserResponseDto.java
+++ src/main/java/com/treasurehunter/treasurehunter/domain/user/dto/UserResponseDto.java
@@ -1,8 +1,13 @@
 package com.treasurehunter.treasurehunter.domain.user.dto;
 
 import com.treasurehunter.treasurehunter.domain.user.domain.User;
+import com.treasurehunter.treasurehunter.global.auth.oauth.dto.UserOauth2AccountsResponseDto;
 import lombok.Builder;
 import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
 public class UserResponseDto {
@@ -15,6 +20,7 @@ public class UserResponseDto {
     private final Integer badgeCount;
     private final Integer totalScore;
     private final Integer totalReviews;
+    private final List<UserOauth2AccountsResponseDto> userOauth2Accounts;
     //각각 구현후 아래에 receivedReviews, reviews, posts, blockedUser 추가하기
 
     @Builder
@@ -28,5 +34,13 @@ public class UserResponseDto {
         this.badgeCount = user.getBadgeCount();
         this.totalScore = user.getTotalScore();
         this.totalReviews = user.getTotalReviews();
+
+        if(user.getUserOauth2Accounts() != null && !user.getUserOauth2Accounts().isEmpty()){
+            this.userOauth2Accounts = user.getUserOauth2Accounts().stream()
+                    .map(UserOauth2AccountsResponseDto::new)
+                    .collect(Collectors.toList());
+        } else{
+            this.userOauth2Accounts = new ArrayList<>();
+        }
     }
 }
