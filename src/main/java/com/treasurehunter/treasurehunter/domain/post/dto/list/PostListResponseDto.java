9ec52c614e840e8955783184e584d34ca35f3544
ggm77 <shmshm86@naver.com>
2025-10-30 20:10:49 +0900

    feat : change post likst response from PostResponseDto to PostSimpleResponseDto

--- src/main/java/com/treasurehunter/treasurehunter/domain/post/dto/list/PostListResponseDto.java
+++ src/main/java/com/treasurehunter/treasurehunter/domain/post/dto/list/PostListResponseDto.java
@@ -2,17 +2,18 @@ package com.treasurehunter.treasurehunter.domain.post.dto.list;
 
 import com.treasurehunter.treasurehunter.domain.post.domain.Post;
 import com.treasurehunter.treasurehunter.domain.post.dto.PostResponseDto;
+import com.treasurehunter.treasurehunter.domain.post.dto.PostSimpleResponseDto;
 import lombok.Getter;
 
 import java.util.List;
 
 @Getter
 public class PostListResponseDto {
-    private List<PostResponseDto> posts;
+    private List<PostSimpleResponseDto> posts;
 
     public PostListResponseDto(final List<Post> posts) {
         this.posts = posts.stream()
-                .map(PostResponseDto::new)
+                .map(PostSimpleResponseDto ::new)
                 .toList();
     }
 }
